fd660feda35ec104b4e0d2cbfd5a949736a3c5c2
Rahul <rahular60@gmail.com>
2020-08-22 16:39:32 +0530

    url routed to all

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,6 +19,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('customer/', views.customer, name='customer'),
+    path('customer/<str:pk_test>/', views.customer, name='customer'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -20,6 +20,11 @@ def products(request):
     products = Product.objects.all()
     return render(request,"products.html",{'products':products})
 
-def customer(request):
-    return render(request,"customer.html")
+def customer(request,pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer':customer,'orders':orders,'order_count':order_count}
+    return render(request,"customer.html",context)
 
